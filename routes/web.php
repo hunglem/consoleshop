69f007728f00f5f96600b80b2d6f9e7795b860dd
hunglem <hungle02042005@gmail.com>
2025-05-07 11:04:30 +0700

    ajustment

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Controller;
 
 Auth::routes();
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
